c38b12a3557358132eaf3e49457f58cdd73f904e
lksdll <121697835+lksdll@users.noreply.github.com>
2024-08-27 00:22:09 +0900

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/FirstAVLTree.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author lukasdoll
+ */
+public class FirstAVLTree {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
